aee99c7d34037eeecb2382aa389344f9a9db5a79
Naira Maximo <naira.maximo@hotmail.com>
2022-09-16 14:41:32 -0300

    alterções git

--- operacao.py
+++ operacao.py
@@ -45,25 +45,6 @@ elif resposta == "s":
 else:
     print("Teste finalizado")
 
-# Elaborar um algoritmo que calcule as raízes da equação do 2º grau conforme a fórmula de Bhaskara.
-
-a = int(input("Insira o valor de a: "))
-b = int(input("Insira o valor de b: "))
-c = int(input("Insira o valor de c: "))
-
-delta = (b ** 2) - (4 * a * c)
-
-if delta < 0:
-    print("Não tem raízes reais")
-elif delta == 0:
-    x = -b / (2 * a)
-    print("Temos como solução duas raízes reais iguais no valor de:", x)
-else:
-    x1 = (-b + math.sqrt(delta)) / (2 * a)
-    x2 = (-b - math.sqrt(delta)) / (2 * a)
-    print("Temos como solução duas raízes reais e diferentes")
-    print(f"x1 = {x1}")
-    print(f"x2 = {x2}")
 
 # Criar um algoritmo que permita fazer a conexão cambial entre Reais e Dólares.
 # Considerar como taxa de câmbio US$1,0 = R$2,40.
@@ -71,26 +52,22 @@ else:
 
 # Exercícios de Fixação em sala 5/9/2022
 
-#Tendo como dados de entrada a altura e o sexo de uma pessoa (M masculino e F feminino)
-# construa um algoritmo que calcule seu peso ideal, utilizando as seguintes fórmulas:
-# * para homens: (72.7*h)-58
-# * para mulheres (62.1*h)-44.7
 
-print("Calculo do peso ideal")
-
-h = float(input("Insira sua altura em metros: "))
-
-pesohomem = (72.7*h)-58
-pesomulher = (62.1*h)-44.7
-
-resposta = input("Qual o seu sexo? (F) Feminino (M) Masculino")
-if resposta == "F":
-    print(f"Seu peso ideal:{pesohomem} ")
-elif resposta == "M":
-    print(f"Seu peso ideal:{pesomulher}")
-else:
-    print("Responda com F para Feminino e M para Masculino")
 
 # Calcular e imprimir o valor a ser pago pelo cliente.
 # Álcool: até 20 litros, desconto de 3% por litro; acima de 20 litros, desconto de 5% por litro
 # Gasolina: até 20 litros, desconto de 4% por litro; acima de 20 litros, desconto de 6% por litro
+
+
+a=int(input("Primeiro valor: "))
+b=int(input("Segundo valor: "))
+if a > b:
+    print("O primeiro valor é maior que o segundo!")
+if b > a:
+    print("O segundo valor é maior que o primeiro!")
+
+v=int(input("Velocidade: "))
+if v > 110:
+    print("Você foi multado!")
+    multa=(v-100)*5
+    print(f"Valor da multa a ser paga é: {multa}") 
