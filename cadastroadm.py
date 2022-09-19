f69e71855a090e90c1fdc4e74ed261dc6bcde054
Naira Maximo <naira.maximo@hotmail.com>
2022-09-19 18:38:18 -0300

    eliminação arquivo email - modificação tela

--- cadastroadm.py
+++ cadastroadm.py
@@ -1,8 +1,8 @@
- # Importar bibliotecas
+# Importar bibliotecas
 import tkinter as tk
 from tkinter import ttk
 from tkinter import END
-from tkinter.messagebox import NO
+from tkinter.messagebox import NO, YES
 from turtle import heading
 import win32com.client as win32  # Biblioteca que possibilita a interagração com o e-mail
 
@@ -18,152 +18,114 @@ def criar_grupo():
     email_client = ent_cemail.get()
     global grupo
     grupo += 1
-    codigo_str = "GRUPO-{}".format(grupo)
-    tree.insert("", END, values=[codigo_str, nome_lider, email_lider, nome_client, email_client])
-    tree.grid(row=0, column=0)
+    codigo_str = 'GRUPO-{}'.format(grupo)
+    tree.insert('', END, values=[codigo_str, nome_lider, email_lider, nome_client, email_client])
+    tree.place(relx=0.02, rely=0.02, relwidth=0.96, relheight=0.96)
+    # Chamar função com os parâmetros "nome" e "email1"
+    enviaremail(ent_lider.get(), ent_lemail.get())
+    enviaremail(ent_client.get(), ent_cemail.get())
 
-window.configure(bg="#fae8e8")  # Cor do plano de fundo da tela
-window.title("Sistema de Cadastro - Administrador")  # Título da janela
+window.configure(bg='#fae8e8')  # Cor do plano de fundo da tela
+window.geometry("1200x600")
+window.title('Sistema de Cadastro - Administrador')  # Título da janela
 
-# Título da página
-lbl_titulo=tk.Label(master=window,
-    text="Cadastro de Grupos",
-    fg="#000000", bg="#fae8e8", font=('Calibre', 30))
-lbl_titulo.grid(row=0, column=0, padx=30, pady=20)
+def textojanela(tipo, texto, tamanho, x, y, largura, altura):
+    tipo=tk.Label(master=window,
+    text=texto, fg='#1a1d1a', bg='#fae8e8', font=('Calibre', tamanho))
+    tipo.place(relx=x, rely=y, relwidth=largura, relheight=altura)
 
-# Widget de texto - descrição do cadastro
-lbl_desc=tk.Label(
-    master=window,
-    text='Após inserir os dados do Líder do Grupo e Fake Client, clique no botão "Cadastrar" para salvar as informações e criar outro grupo.',
-    fg="#000000", bg="#fae8e8", font=('Calibre', 15))
-lbl_desc.grid(row=1, column=0, padx=30)
-
-# Widget de texto - descrição do envio de senha
-lbl_senha=tk.Label(
-    master=window,
-    text='Uma senha gerada automaticamente será enviada para o e-mail de cada um dos integrantes ao final do cadastro.',
-    fg="#000000", bg="#fae8e8", font=('Calibre', 13))
-lbl_senha.grid(row=2, column=0, padx=30)
+textojanela('lbl_titulo', 'Cadastro de Grupos', 30, 0.02, 0.01, 0.96, 0.09)
+textojanela('lbl_desc', 
+    'Após inserir os dados do Líder do Grupo e Fake Client, clique no botão "Cadastrar" para salvar as informações e criar outro grupo.', 
+    15, 0.02, 0.1, 0.96, 0.06)  
+textojanela('lbl_senha', 
+    'Uma senha gerada automaticamente será enviada para o e-mail de cada um dos integrantes ao final do cadastro.', 
+    15, 0.02, 0.17, 0.96, 0.04)
 
 # Frame do cadastro de grupos
-frm_grupo=tk.Frame(master=window, relief=tk.GROOVE, bd=3,
-    width=1000, height=100, bg="#fae8e8")
-frm_grupo.rowconfigure([0, 1], weight=1, minsize=50) 
+frm_grupo=tk.Frame(master=window, relief=tk.GROOVE, bd=3, bg='#fae8e8')
+frm_grupo.rowconfigure([0, 1], weight=1, minsize=30) 
 frm_grupo.columnconfigure([0, 1, 2, 3, 4], weight=1, minsize=100)
-frm_grupo.grid(row=3, column=0, padx=30, pady=30)
+frm_grupo.place(relx=0.02, rely=0.22, relwidth=0.96, relheight=0.2)
 
 # Frame da tabela de valores cadastrados
-frm_tabela=tk.Frame(master=window, relief=tk.FLAT, bd=1,
-    width=100, height=100, bg="#fae8e8")
+frm_tabela=tk.Frame(master=window, relief=tk.GROOVE, bd=1, bg='#fae8e8')
 frm_tabela.rowconfigure(0, weight=1, minsize=100) 
 frm_tabela.columnconfigure(0, weight=1, minsize=100)
-frm_tabela.grid(row=4, column=0, padx=5, pady=5)
+frm_tabela.place(relx=0.02, rely=0.45, relwidth=0.96, relheight=0.5)
 
-# Widget de texto - nome Líder do Grupo
-lbl_lider=tk.Label(master=frm_grupo, text="Nome do Líder do Grupo:",
-    fg="#000000", bg="#fae8e8", font=('Calibre', 15))
-lbl_lider.grid(row=0, column=0, sticky="e")
+# Widgets de entrada
+ent_lider=tk.Entry(master=frm_grupo, width=30, fg='#1a1d1a', font=('Calibre 13'))  # Nome Líder
+ent_lider.grid(row=0, column=1, padx=5)
 
-# Widget de entrada - nome Líder do Grupo
-ent_lider=tk.Entry(master=frm_grupo, width=30, fg="#000000", font=('Calibre 13'))
-ent_lider.grid(row=0, column=1, padx=10)
+ent_lemail=tk.Entry(master=frm_grupo, width=30, fg='#1a1d1a', font=('Calibre 13'))  # E-mail Líder
+ent_lemail.grid(row=0, column=3, padx=5)
 
-# Widget de texto - e-mail Líder do Grupo
-lbl_lemail=tk.Label(master=frm_grupo, text="E-mail do Líder do Grupo:",
-    fg="#000000", bg="#fae8e8", font=('Calibre', 15))
-lbl_lemail.grid(row=0, column=2, sticky="e")
+ent_client=tk.Entry(master=frm_grupo, width=30, fg='#1a1d1a', font=('Calibre 13'))  # Nome Client
+ent_client.grid(row=1, column=1, padx=5)
 
-# Widget de entrada - e-mail Líder do Grupo
-ent_lemail=tk.Entry(master=frm_grupo, width=30, fg="#000000", font=('Calibre 13'))
-ent_lemail.grid(row=0, column=3, padx=10)
+ent_cemail=tk.Entry(master=frm_grupo, width=30, fg='#1a1d1a', font=('Calibre 13'))  # E-mail Client
+ent_cemail.grid(row=1, column=3, padx=5)
 
-# Widget de texto - nome Fake Client
-lbl_client=tk.Label(master=frm_grupo, text="Nome do Fake Client:",
-    fg="#000000", bg="#fae8e8", font=('Calibre', 15))
-lbl_client.grid(row=1, column=0, sticky="e")
+# Função para widget de texto
+def widgetlabel(usuario, linha, coluna, text):
+    usuario=tk.Label(master=frm_grupo, text=text,
+        fg='#1a1d1a', bg='#fae8e8', font=('Calibre', 15))
+    usuario.grid(row=linha, column=coluna, sticky='e')
 
-# Widget de entrada - nome Fake Client
-ent_client=tk.Entry(master=frm_grupo, width=30, fg="#000000", font=('Calibre 13'))
-ent_client.grid(row=1, column=1, padx=10)
+widgetlabel('lbl_lider', 0, 0, 'Nome do Líder do Grupo:')  # Widget de texto nome Líder do Grupo
+widgetlabel('lbl_lemail', 0, 2, 'E-mail do Líder do Grupo:')  # Widget de texto e-mail Líder do Grupo
+widgetlabel('lbl_client', 1, 0, 'Nome do Fake Client:') # Widget de texto nome Fake Client
+widgetlabel('lbl_cemail', 1, 2, 'E-mail do Fake Client:')  # Widget de texto e-mail Fake Client
 
-# Widget de texto - e-mail Fake Client
-lbl_cemail=tk.Label(master=frm_grupo, text="E-mail do Fake Client:",
-    fg="#000000", bg="#fae8e8", font=('Calibre', 15))
-lbl_cemail.grid(row=1, column=2, sticky="e")
-
-# Widget de entrada - e-mail Fake Client
-ent_cemail=tk.Entry(master=frm_grupo, width=30, fg="#000000", font=('Calibre 13'))
-ent_cemail.grid(row=1, column=3, padx=10)
-
-# Botão de cadastro
-button_grupo=tk.Button(master=frm_grupo, text="Cadastrar", 
-    fg="#000000", bg="#a6a6a6", font=('Calibre', 15),
-    width=10, height=1, activebackground="#c5a8b0",
-    command=criar_grupo)
-button_grupo.grid(row=0, column=4, padx=20)
-
-# Botão tela Home
-button_home=tk.Button(master=frm_grupo, text="Home", 
-    fg="#000000", bg="#a6a6a6", font=('Calibre', 15),
-    width=10, height=1,
-    activebackground="#c5a8b0")
-button_home.grid(row=1, column=4, padx=20)
+def criarbotao(nome, texto, comando, linha):
+    nome=tk.Button(master=frm_grupo, text=texto, 
+    fg='#1a1d1a', bg='#d9d9d9', font=('Calibre', 15),
+    width=10, height=1, activebackground='#c5a8b0',
+    command=comando)
+    nome.grid(row=linha, column=4, padx=5, pady=5)
+criarbotao('button_grupo', 'Cadastrar', criar_grupo, 0)
+criarbotao('button_home', 'Home', 0, 1)
 
 # Tabela gerada com dados cadastrados pelo Administrador
-tree=ttk.Treeview(
-    master=frm_tabela,
-    selectmode="browse",
-    column=("codigogrupo", "nomelider",
-    "emaillider", "nomeclient", "emailclient"),
+tree=ttk.Treeview(master=frm_tabela, selectmode='browse',
+    column=('codigogrupo', 'nomelider', 'emaillider', 'nomeclient', 'emailclient'),
     show="headings")
 
-tree.column("codigogrupo", width=100, minwidth=50, stretch=NO)
-tree.heading("#1", text="Código do grupo")
+def criartabela(coluna, número, texto): 
+    tree.column(coluna, width=200, minwidth=50, stretch=YES)
+    tree.heading(número, text=texto)
 
-tree.column("nomelider", width=200, minwidth=50, stretch=NO)
-tree.heading("#2", text="Nome do Líder do Grupo")
+criartabela('codigogrupo', '#1', 'Código do grupo')
+criartabela('nomelider', '#2', 'Nome do Líder do Grupo')
+criartabela('emaillider', '#3', 'E-mail do Líder do Grupo')
+criartabela('nomeclient', '#4', 'Nome do Fake Client')
+criartabela('emailclient', '#5', 'E-mail do Fake Client')
 
-tree.column("emaillider", width=200, minwidth=50, stretch=NO)
-tree.heading("#3", text="E-mail do Líder do Grupo")
+# Função envio de  e-mail Líder do Grupo e Fake Client
+def enviaremail(nome, email1):
+    outlook = win32.Dispatch('outlook.application')  # Criar interação com outlook
+    email = outlook.CreateItem(0)  # Criar um e-mail
 
-tree.column("nomeclient", width=200, minwidth=50, stretch=NO)
-tree.heading("#4", text="Nome do Fake Client")
+    # Configurar as informações do e-mail
+    email.To = email1
+    email.Subject = 'E-mail automático - Senha cadastrada para Avaliação 360°'
+    email.HTMLBody = f"""
+    <p>Olá, {nome}!</p>
 
-tree.column("emailclient", width=200, minwidth=50, stretch=NO)
-tree.heading("#5", text="E-mail do Fake Client")
+    <p>Aqui está sua senha gerada automaticamente, para acesso à plataforma de Avaliação 360°:</p>
+    <p>SENHA:</p>
+    <p>E-MAIL CADASTRADO: {email1}</p>
 
-# Criar interação com outlook
-outlook = win32.Dispatch('outlook.application')
+    <p><b>Sua senha é intrasferível, não compartilhe com ninguém.</b></p>
 
-# Criar um e-mail
-email = outlook.CreateItem(0)
+    <p>Não responda a este e-mail.</p>
+    """
 
-# Configuraras informações do seu e-mail
-email_lider = ent_lemail.get()
-email_client = ent_cemail.get()
-
-email.To = email_lider
-email.Subject = 'E-mail automático - Senha cadastrada para Avaliação 360°'
-email.HTMLBody = f"""
-<p>Olá, usuário!</p>
-
-<p>Aqui está sua senha gerada automaticamente, para acesso à plataforma de Avaliação 360°:</p>
-<p>E-MAIL CADASTRADO:</p>
-<p>SENHA:</p>
-
-<p>*Sua senha é intrasferível, não compartilhe com ninguém.*</p>
-
-<p>Não responda a este e-mail.</p>
-"""
-
-enviar1 = email.To
-assunto1 = email.Subject
-corpo1 = email.HTMLBody
-
-if str(object='@') in email_lider:
-    email.Send()
-    print("E-mail Enviado")
-else:
-        print('não existe e-mail cadastrado')
+    if str(object='@') in email1:
+        email.Send()
+        print(f'E-mail enviado para {nome}')
+    else:
+        print(f'Não existe e-mail cadastrado para {nome}')
 
 window.mainloop()  # Método que executa eventos como cliques de botão e mantém a janela aberta
